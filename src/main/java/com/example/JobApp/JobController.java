51604f70b784f495d3433179451bf4aeedc5c6db
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2024-08-08 21:41:23 +0200

    the app is now in service

--- src/main/java/com/example/JobApp/JobController.java
+++ src/main/java/com/example/JobApp/JobController.java
@@ -1,16 +1,71 @@
 package com.example.JobApp;
 
+import com.example.JobApp.model.JobPost;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
 
 @Controller
 public class JobController {
-    @RequestMapping({"/", "home"})
+
+    @Autowired
+    private JobService service;
+
+    // ************************************************************************
+
+    @RequestMapping({"/","/home"})
     public String home() {
         return "home";
     }
-    @RequestMapping("addjob")
-    public String addJob(){
+
+    // ************************************************************************
+
+    @RequestMapping("/addjob")
+    public String addJob() {
         return "addjob";
     }
+
+    // ************************************************************************
+
+    // controller method for getting all job posts
+    @GetMapping("/viewalljobs")
+    public String viewJobs(Model model) {
+
+        List<JobPost> jobPosts = service.returnAllJobPosts();
+        model.addAttribute("jobPosts", jobPosts);
+        return "viewalljobs";
+    }
+
+    // ************************************************************************
+
+    @PostMapping("/handleForm")
+    public String handleAddJobForm(JobPost jobPost,Model model) {
+        model.addAttribute("jobPost", jobPost);
+        service.addJobPost(jobPost);
+        //System.out.println(jobPost);
+        return "success";
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
